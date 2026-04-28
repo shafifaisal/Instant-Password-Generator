9297139a20cd582d5cbca22558c74162c8ac37fb
Faisal Shafi <61265568+shafifaisal@users.noreply.github.com>
2026-04-28 13:52:18 +0500

    Update home route to use full view path

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('resources/views/index');
 })->name('home');
 
 Route::view('/about', 'pages.about')->name('about');
